e977e38ee8cb498708c036dabccc831a4007ab7a
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-13 08:31:35 -0500

    Latitud longitud solo en html

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public latitud;
+  public longitud;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {     
+     let watch = this.geolocation.watchPosition();
+     watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+        console.log(data);
+        console.log(data);
+     });
+     console.log("XXXXXXXX");
   }
 
+  ionViewDidLoad(){
+  }
+
+  getLatitudeLongitude() {
+    console.log("1 ++++++++++++++++++++++++++");
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log(resp.coords.latitude);
+      console.log(resp.coords.longitude);
+      this.latitud = resp.coords.latitude;
+      this.longitud = resp.coords.longitude;
+    }).catch((error) => {
+      console.log(error);
+      console.log('Error getting location', error);
+    });
+    console.log("2 ++++++++++++++++++++++++++");
+  }
 }
